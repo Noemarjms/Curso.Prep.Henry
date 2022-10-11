49e2198e89c40bd77875db279e8f953c3b95150e
Noemarjms <Noemarjms@gmail.com>
2022-10-11 20:23:06 -0300

    tarea2 sin finalizar

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -176,11 +176,9 @@ function esPrimo(numero) {
 for (var i=2; i<numero; i++)
   if (numero%i===1){
     return true;
-}else { 
-  if (numero%i!==1){
-return false;
-  } else;
-}
+}else 
+  return false;
+
 }
 
 
